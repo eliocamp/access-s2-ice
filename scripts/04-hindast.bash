f2c9e4ccfb19ea17c6fd921a0c32a397dc990f68
Elio Campitelli <eliocampitelli@gmail.com>
2024-07-01 13:45:12 +1000

    Corrrects persistence

--- scripts/04-hindast.bash
+++ scripts/04-hindast.bash
@@ -8,40 +8,36 @@ temp_nsidc=$(mktemp)
 for ensemble in "01" "02" "03"; do
     dir=/g/data/ux62/access-s2/hindcast/raw_model/ice/aice/monthly/e$ensemble
     for file in $dir/mi_aice_??????01_e${ensemble}.nc; do
+        echo "------------ $(basename $file .nc) ------------"
         correlation=$data_derived/correlations/$(basename $file .nc)_forecast.nc
         correlation_persistence=$data_derived/correlations/$(basename $file .nc)_persistence.nc
 
         rmse=$data_derived/rmse/$(basename $file .nc)_forecast.nc
         rmse_persistence=$data_derived/rmse/$(basename $file .nc)_persistence.nc
-
-        # Hay que buscar en /g/data/ux62/access-s2/reanalysis/ice/aice/di_aice_YYYY.nc y seleccionar el primero del mes.
-        start_date=$(cdo -s showdate $file | awk '{print $1}')
-        day=1
-        year=$(echo $start_date | cut -d"-" -f1)
-        month=$(echo $start_date | cut -d"-" -f2)
-        start_date=$year-$month-$day
-        #
-        if [ $day$month -eq "0101" ]; then
-            year=$(($year - 1))
-        fi
-        file_daily=/g/data/ux62/access-s2/reanalysis/ice/aice/di_aice_$year.nc
-
-        # I think this is only for the first timestep (1981-01-01 doesn't exist in the reanalysis??)
-        if [[ ! -f $file_daily ]]; then
-            continue
-        fi
+        rmse_persistence_month=$data_derived/rmse/$(basename $file .nc)_persistence_month.nc
 
         for file2 in $correlation $correlation_persistence $rmse $rmse_persistence; do
             if [[ ! -f $file2 ]]; then
-                echo "------ Cutting and remapping ------"
-                cdo -L remapbil,$nsidc_data -setday,1 -ymonsub -sellonlatbox,0,360,-90,$top_lat -selname,aice $file $access_climatology $temp_access_anom
+                # Hay que buscar en /g/data/ux62/access-s2/hindcast/raw_model/ice/aice/daily/
+                start_date=$(cdo -s showdate $file | awk '{print $1}')
+                day=01
+                year=$(echo $start_date | cut -d"-" -f1)
+                month=$(echo $start_date | cut -d"-" -f2)
 
-                cdo -L ydaysub -remapbil,$nsidc_data -sellonlatbox,0,360,-90,$top_lat -seldate,$start_date $file_daily $access_climatology_daily $first_day
+                file_daily=/g/data/ux62/access-s2/hindcast/raw_model/ice/aice/daily/e$ensemble/di_aice_$year$month${day}_e$ensemble.nc
+                start_date=$year-$month-$day
+
+                echo "------ Cutting and remapping ------"
+                cdo -L remapbil,$nsidc_anomaly  -setday,1 -ymonsub -sellonlatbox,0,360,-90,$top_lat -selname,aice $file $access_climatology $temp_access_anom
+
+                echo "------ Cutting and remapping persistence ------"
+                cdo -L -setday,1 -ydaysub -remapbil,$nsidc_anomaly -sellonlatbox,0,360,-90,$top_lat -seldate,$year-$month-02 -selname,aice $file_daily -seldate,2000-$month-02 $access_climatology_daily $first_day
                 cdo -L mul -expr,"aice=1" $temp_access_anom $first_day $persistence
 
+                echo "------ Selecting dates in nsidc ------"
                 start_date=$(cdo -s showdate $temp_access_anom | awk '{print $1}')
                 end_date=$(cdo -s showdate $temp_access_anom | awk '{print $NF}')
-                cdo -L seldate,$start_date,$end_date $nsidc_data $temp_nsidc
+                cdo -L seldate,$start_date,$end_date $nsidc_anomaly $temp_nsidc
                 break
             fi
         done
