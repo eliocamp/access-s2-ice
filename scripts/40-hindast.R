ec93a9bb3e529f318f22e019a3b4b36c91417b30
Elio Campitelli <eliocampitelli@gmail.com>
2024-09-19 16:40:18 +1000

    Reformats

--- scripts/40-hindast.R
+++ scripts/40-hindast.R
@@ -13,26 +13,28 @@ library(progressr)
 source("scripts/setup/functions.R")
 
 nsidc_anomaly <- globals$nsidc_daily_anomaly
-nsidc_data  <- globals$nsidc_daily_data
+nsidc_data <- globals$nsidc_daily_data
 climatology <- globals$access_reanalysis_climatology_daily
 nsidc_climatology <- globals$nsidc_climatology_daily
 
 
 cdo_del29feb <- function(input, output = NULL) {
-    del29feb <- list(command = "del29feb", 
-                 params = NULL,
-                 n_input = 1, 
-                 n_output = 1)
-    rcdo::cdo(operator = del29feb, input = list(input), output = output) 
+  del29feb <- list(
+    command = "del29feb",
+    params = NULL,
+    n_input = 1,
+    n_output = 1
+  )
+  rcdo::cdo(operator = del29feb, input = list(input), output = output)
 }
 
-climatology_twice <-  cdo_mergetime(list(cdo_setyear(nsidc_climatology, 2001), cdo_setyear(nsidc_climatology, 2002))) |> 
-    cdo_del29feb() |>
-    cdo_execute(options = "-L")
+climatology_twice <- cdo_mergetime(list(cdo_setyear(nsidc_climatology, 2001), cdo_setyear(nsidc_climatology, 2002))) |>
+  cdo_del29feb() |>
+  cdo_execute(options = "-L")
 
 
-dates  <- seq(as.Date("1981-01-01"), as.Date("2018-12-01"), by = "month")
-members  <- paste0("0", 1:9)
+dates <- seq(as.Date("1981-01-01"), as.Date("2018-12-01"), by = "month")
+members <- paste0("0", 1:9)
 
 dir <- "/g/data/ux62/access-s2/hindcast/raw_model/ice/aice/daily/"
 
@@ -43,224 +45,237 @@ iiee_dir <- file.path(globals$data_derived, "iiee")
 dir.create(iiee_dir, showWarnings = FALSE)
 
 
-files <- CJ(time = dates, member = members) |> 
-    _[, file := paste0(dir, "e", member, "/di_aice_", format(time, "%Y%m%d"), "_e", member, ".nc")] |> 
-    _[] |> 
-    as.data.frame()
+files <- CJ(time = dates, member = members) |>
+  _[, file := paste0(dir, "e", member, "/di_aice_", format(time, "%Y%m%d"), "_e", member, ".nc")] |>
+  _[] |>
+  as.data.frame()
 
-files <- rbind(files, 
- CJ(time = dates, member = "mm")  |> 
-    _[, file := file.path(globals$data_derived, "hindcast_ensmean", 
-                         paste0("di_aice_", format(time, "%Y%m%d"), "_e", member, ".nc"))]  
+files <- rbind(
+  files,
+  CJ(time = dates, member = "mm") |>
+    _[, file := file.path(
+      globals$data_derived, "hindcast_ensmean",
+      paste0("di_aice_", format(time, "%Y%m%d"), "_e", member, ".nc")
+    )]
 )
 
 cdo_rmse <- function(file1, file2) {
-    cdo_sub(file1, file2) |> 
-        cdo_sqr() |> 
-        cdo_fldmean() |> 
-        cdo_sqrt()
+  cdo_sub(file1, file2) |>
+    cdo_sqr() |>
+    cdo_fldmean() |>
+    cdo_sqrt()
 }
 
 
 cdo_persist <- function(file, file2, init_time) {
-    first_day <- cdo_seldate(file, startdate = as.character(init_time)) |> 
-        cdo_execute(output = tempfile(pattern = basename(file2)))
+  first_day <- cdo_seldate(file, startdate = as.character(init_time)) |>
+    cdo_execute(output = tempfile(pattern = basename(file2)))
 
-    out <- file2 |>
-        cdo_expr(instr = "aice=1") |> 
-        cdo_mul(first_day) |> 
-        cdo_execute(output = tempfile(pattern = basename(file2)))
-    
-    file.remove(first_day)
-    return(out)
+  out <- file2 |>
+    cdo_expr(instr = "aice=1") |>
+    cdo_mul(first_day) |>
+    cdo_execute(output = tempfile(pattern = basename(file2)))
+
+  file.remove(first_day)
+  return(out)
 }
 
 cdo_climextrapolate <- function(climatology_twice, first_time, last_time) {
-    dif <- 2001 - year(first_time)
-    
-    year(first_time) <- year(first_time) + dif
-    year(last_time) <- year(last_time) + dif
+  dif <- 2001 - year(first_time)
 
-    climatology_twice |> 
-        cdo_seldate(paste(first_time, last_time, sep = ",")) |> 
-        cdo_execute()
+  year(first_time) <- year(first_time) + dif
+  year(last_time) <- year(last_time) + dif
+
+  climatology_twice |>
+    cdo_seldate(paste(first_time, last_time, sep = ",")) |>
+    cdo_execute()
 }
 
 remove_if_exists <- function(variable_names) {
-    vapply(variable_names, \(x) {
-        if (exists(x) && file.exists(get(x))) {
-            return(file.remove(get(x)))
-        }
+  vapply(variable_names, \(x) {
+    if (exists(x) && file.exists(get(x))) {
+      return(file.remove(get(x)))
+    }
 
-        return(FALSE)
-    }, FUN.VALUE = logical(1))    
+    return(FALSE)
+  }, FUN.VALUE = logical(1))
 }
 
 compute_metrics <- function(i) {
-    # message(i)
-    cdo_options_set(c("-L", "--pedantic"))
-    file <- files[i, ]
+  # message(i)
+  cdo_options_set(c("-L", "--pedantic"))
+  file <- files[i, ]
 
-    rmse_out <- file.path(rmse_dir, gsub("\\.nc", "_forecast.nc", basename(file$file)))
-    iiee_out <- file.path(iiee_dir, gsub("\\.nc", "_forecast.nc", basename(file$file)))
+  rmse_out <- file.path(rmse_dir, gsub("\\.nc", "_forecast.nc", basename(file$file)))
+  iiee_out <- file.path(iiee_dir, gsub("\\.nc", "_forecast.nc", basename(file$file)))
 
-    rmse_out_persistence <- file.path(rmse_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_persistence.nc", basename(file$file), perl = TRUE))
-    iiee_out_persistence <- file.path(iiee_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_persistence.nc", basename(file$file), perl = TRUE))
+  rmse_out_persistence <- file.path(rmse_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_persistence.nc", basename(file$file), perl = TRUE))
+  iiee_out_persistence <- file.path(iiee_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_persistence.nc", basename(file$file), perl = TRUE))
 
-    rmse_out_climatology <- file.path(rmse_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_climatology.nc", basename(file$file), perl = TRUE))
-    iiee_out_climatology  <- file.path(iiee_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_climatology.nc", basename(file$file), perl = TRUE))
+  rmse_out_climatology <- file.path(rmse_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_climatology.nc", basename(file$file), perl = TRUE))
+  iiee_out_climatology <- file.path(iiee_dir, gsub("\\_e[\\da-z]{2}.nc", "_e01_climatology.nc", basename(file$file), perl = TRUE))
 
-    if (all(file.exists(c(rmse_out_persistence, rmse_out, iiee_out, iiee_out_persistence, rmse_out_climatology, iiee_out_climatology)))) {
-        return()
+  if (all(file.exists(c(rmse_out_persistence, rmse_out, iiee_out, iiee_out_persistence, rmse_out_climatology, iiee_out_climatology)))) {
+    return()
+  }
+
+  ff <- c(rmse_out_persistence, rmse_out, iiee_out, iiee_out_persistence, rmse_out_climatology, iiee_out_climatology)
+
+  stopifnot(!any(grepl("e\\d{2}.nc", ff)))
+
+  nc <- ncdf4::nc_open(file$file)
+  times <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
+  ncdf4::nc_close(nc)
+
+  last_time <- max(times)
+  first_time <- min(times)
+
+  nsidc_data_part <- cdo_seldate(nsidc_data,
+    startdate = as.character(first_time),
+    enddate = as.character(last_time)
+  ) |>
+    cdo_del29feb() |>
+    cdo_execute(output = tempfile(pattern = basename(file$file)))
+
+  nsidc_anomaly_part <- nsidc_data_part |>
+    cdo_ydaysub(globals$nsidc_climatology_daily) |>
+    cdo_execute()
+
+  nc <- ncdf4::nc_open(nsidc_data_part)
+  times_nsidc <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
+  ncdf4::nc_close(nc)
+
+  remap_file <- tempfile(pattern = basename(file$file))
+
+  remapped <- function() {
+    if (file.exists(remap_file)) {
+      return(remap_file)
     }
 
-    ff <- c(rmse_out_persistence, rmse_out, iiee_out, iiee_out_persistence, rmse_out_climatology, iiee_out_climatology)
-    
-    stopifnot(!any(grepl("e\\d{2}.nc", ff)))
+    remap_file <- file$file |>
+      cdo_selname("aice") |>
+      cdo_remap_nsidc() |>
+      cdo_del29feb() |>
+      cdo_execute(output = remap_file)
 
-    nc <- ncdf4::nc_open(file$file)
-    times <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
+    nc <- ncdf4::nc_open(remap_file)
+    times_access <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
     ncdf4::nc_close(nc)
-    
-    last_time <- max(times)
-    first_time <- min(times)
 
-    nsidc_data_part <- cdo_seldate(nsidc_data, 
-                              startdate = as.character(first_time), 
-                              enddate = as.character(last_time)) |> 
-                    cdo_del29feb() |> 
-                    cdo_execute(output = tempfile(pattern = basename(file$file)))                    
+    if (length(times_access) != length(times_nsidc)) {
+      stop("malos tiempos en ", i)
+    }
 
-    nsidc_anomaly_part <- nsidc_data_part |> 
-        cdo_ydaysub(globals$nsidc_climatology_daily) |> 
-        cdo_execute()
+    return(remap_file)
+  }
 
-    nc <- ncdf4::nc_open(nsidc_data_part)
-    times_nsidc <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
+  if (!file.exists(rmse_out)) {
+    remapped() |>
+      cdo_ydaysub(climatology) |>
+      cdo_rmse(nsidc_anomaly_part) |>
+      cdo_execute(output = rmse_out)
+  }
+
+  if (!file.exists(iiee_out)) {
+    cdo_iiee(remapped(), nsidc_data_part, output = iiee_out)
+  }
+
+  if (!file.exists(rmse_out_persistence)) {
+    persistence <- cdo_persist(globals$nsidc_daily_anomaly, nsidc_anomaly_part, init_time = as.character(file$time))
+
+    nc <- ncdf4::nc_open(persistence)
+    times_persistence <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
     ncdf4::nc_close(nc)
 
-    remap_file <- tempfile(pattern = basename(file$file))
-
-    remapped <- function() {   
-        if (file.exists(remap_file)) {
-            return(remap_file)
-        }
-
-        remap_file <- file$file |> 
-                cdo_selname("aice") |> 
-                cdo_remap_nsidc() |> 
-                cdo_del29feb() |> 
-                cdo_execute(output = remap_file)
-        
-        nc <- ncdf4::nc_open(remap_file)
-        times_access <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
-        ncdf4::nc_close(nc)
-
-        if (length(times_access) != length(times_nsidc)) {
-            stop("malos tiempos en ", i)
-        }
-
-        return(remap_file)
-    }
-    
-    if (!file.exists(rmse_out)) {
-        remapped() |> 
-            cdo_ydaysub(climatology) |>        
-            cdo_rmse(nsidc_anomaly_part) |> 
-            cdo_execute(output = rmse_out)
+    if (length(times_persistence) != length(times_nsidc)) {
+      stop("malos tiempos en la persistencia de ", i)
     }
 
-    if (!file.exists(iiee_out)) {
-        cdo_iiee(remapped(), nsidc_data_part, output = iiee_out) 
+    rmse_persistence <- cdo_rmse(persistence, nsidc_anomaly_part) |>
+      cdo_execute(output = rmse_out_persistence)
+  }
+
+  if (!file.exists(iiee_out_persistence)) {
+    if (!exists(persistence)) {
+      persistence <- cdo_persist(globals$nsidc_daily_data, nsidc_data_part, init_time = as.character(file$time))
     }
 
-    if (!file.exists(rmse_out_persistence)) {
-        persistence <- cdo_persist(globals$nsidc_daily_anomaly, nsidc_anomaly_part, init_time = as.character(file$time)) 
+    iiee_persistence <- cdo_iiee(persistence, nsidc_data_part, output = iiee_out_persistence)
+  }
 
-        nc <- ncdf4::nc_open(persistence)
-        times_persistence <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
-        ncdf4::nc_close(nc)
+  if (!file.exists(rmse_out_climatology)) {
+    clim <- cdo_climextrapolate(climatology_twice, first_time, last_time)
 
-        if (length(times_persistence) != length(times_nsidc)) {
-            stop("malos tiempos en la persistencia de ", i)
-        }
+    nc <- ncdf4::nc_open(clim)
+    times_clim <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
+    ncdf4::nc_close(nc)
 
-        rmse_persistence <- cdo_rmse(persistence, nsidc_anomaly_part) |> 
-            cdo_execute(output = rmse_out_persistence)        
+    if (length(times_clim) != length(times_nsidc)) {
+      stop("malos tiempos en la climatología de ", i)
     }
 
-    if (!file.exists(iiee_out_persistence)) {
-        if (!exists(persistence)) {
-            persistence <- cdo_persist(globals$nsidc_daily_data, nsidc_data_part, init_time = as.character(file$time))
-        }
-        
-        iiee_persistence <- cdo_iiee(persistence, nsidc_data_part, output = iiee_out_persistence)
-    }
+    cdo_iiee(nsidc_data_part, clim, output = iiee_out_climatology)
+  }
 
-    if (!file.exists(rmse_out_climatology)) {
-        clim  <- cdo_climextrapolate(climatology_twice, first_time, last_time)
+  file.remove(c(remap_file, nsidc_data_part, nsidc_anomaly_part))
 
-        nc <- ncdf4::nc_open(clim)
-        times_clim <- metR:::.parse_time(time = nc$dim$time$vals, units = nc$dim$time$units, calendar = nc$dim$time$calendar)
-        ncdf4::nc_close(nc)
-
-        if (length(times_clim) != length(times_nsidc)) {
-            stop("malos tiempos en la climatología de ", i)
-        }
-                
-        cdo_iiee(nsidc_data_part, clim, output = iiee_out_climatology)
-    }
-
-    file.remove(c(remap_file, nsidc_data_part, nsidc_anomaly_part))
-    
-    remove_if_exists(c("persistence", "clim"))
+  remove_if_exists(c("persistence", "clim"))
 }
 
 
 out <- furrr::future_map(seq_len(nrow(files)), compute_metrics)
 
-read_measures <- function(files) {  
+read_measures <- function(files) {
   dates <- utils::strcapture("di_aice_(\\d{8})_e([\\dm]{2})_(\\w+).nc", basename(files),
-                             proto = list(time_forecast = character(),
-                                          member = character(),
-                                          type = character()), perl = TRUE) |> 
-    as.data.table() |> 
-    _[, time_forecast := as.Date(time_forecast, format = "%Y%m%d")] |> 
+    proto = list(
+      time_forecast = character(),
+      member = character(),
+      type = character()
+    ), perl = TRUE
+  ) |>
+    as.data.table() |>
+    _[, time_forecast := as.Date(time_forecast, format = "%Y%m%d")] |>
     _[]
-  
+
   read <- function(i) {
     data <- try(metR::ReadNetCDF(files[[i]], vars = c(value = "aice")), silent = TRUE)
-    
+
     if (inherits(data, "try-error")) {
       file.remove(files[[i]])
       warning("file ", basename(files[[i]]), " deleted")
       return(NULL)
     }
 
-    data |> 
-      _[, let(lat = NULL, lon = NULL)] |> 
-      _[] |> 
-      _[, let(time_forecast = dates$time_forecast[[i]],
-              member = dates$member[[i]],
-              type = dates$type[[i]],
-              file = files[[i]])] |> 
-      _[] 
+    data |>
+      _[, let(lat = NULL, lon = NULL)] |>
+      _[] |>
+      _[, let(
+        time_forecast = dates$time_forecast[[i]],
+        member = dates$member[[i]],
+        type = dates$type[[i]],
+        file = files[[i]]
+      )] |>
+      _[]
   }
-  
-  furrr::future_map(seq_along(files), read) |> 
-    rbindlist() 
+
+  furrr::future_map(seq_along(files), read) |>
+    rbindlist()
 }
 
 
-list.files(file.path(globals$data_derived, "rmse"), pattern = "di_aice_",
-                          full.names = TRUE) |> 
-            read_measures() |>
-            _[, measure := "rmse"]  |> 
-            saveRDS(file.path(globals$data_derived, "rmse.Rds"))
+list.files(file.path(globals$data_derived, "rmse"),
+  pattern = "di_aice_",
+  full.names = TRUE
+) |>
+  read_measures() |>
+  _[, measure := "rmse"] |>
+  saveRDS(file.path(globals$data_derived, "rmse.Rds"))
 
 
-list.files(file.path(globals$data_derived, "iiee"), pattern = "di_aice_",
-                          full.names = TRUE) |> 
-            read_measures() |>
-            _[, measure := "iiee"]  |> 
-            saveRDS(file.path(globals$data_derived, "iiee.Rds"))
+list.files(file.path(globals$data_derived, "iiee"),
+  pattern = "di_aice_",
+  full.names = TRUE
+) |>
+  read_measures() |>
+  _[, measure := "iiee"] |>
+  saveRDS(file.path(globals$data_derived, "iiee.Rds"))
--- scripts/setup/functions.R
+++ scripts/setup/functions.R
@@ -1,51 +1,49 @@
-
-
 get_envars <- function(file = here::here("scripts/setup/variables.bash")) {
-        old <- Sys.getenv()
-        readRenviron(file)
-        new <- Sys.getenv()
-        new_names <- names(new)[!(names(new) %in% names(old))]
-        Sys.unsetenv(new_names)
+  old <- Sys.getenv()
+  readRenviron(file)
+  new <- Sys.getenv()
+  new_names <- names(new)[!(names(new) %in% names(old))]
+  Sys.unsetenv(new_names)
 
-        as.list(new[!(names(new) %in% names(old))])
+  as.list(new[!(names(new) %in% names(old))])
 }
 
 globals <- get_envars()
 
 cdo_remap_nsidc <- function(ifile, ofile = NULL) {
-        op <- rcdo::cdo_operator(command = globals$cdo_remap_nsidc, params = NULL, n_input = 1, n_output = 1)
-        rcdo::cdo(op,  input = list(ifile), params = NULL, output = ofile)
+  op <- rcdo::cdo_operator(command = globals$cdo_remap_nsidc, params = NULL, n_input = 1, n_output = 1)
+  rcdo::cdo(op, input = list(ifile), params = NULL, output = ofile)
 }
 
 cdo_extent <- function(ifile, ofile = NULL) {
-        op <- rcdo::cdo_operator(command = globals$cdo_extent, params = NULL, n_input = 1, n_output = 1)
-        rcdo::cdo(op,  input = list(ifile), params = NULL, output = ofile)
+  op <- rcdo::cdo_operator(command = globals$cdo_extent, params = NULL, n_input = 1, n_output = 1)
+  rcdo::cdo(op, input = list(ifile), params = NULL, output = ofile)
 }
 
-cdo_area<- function(ifile, ofile = NULL) {
-        op <- rcdo::cdo_operator(command = globals$cdo_area, params = NULL, n_input = 1, n_output = 1)
-        rcdo::cdo(op,  input = list(ifile), params = NULL, output = ofile)
+cdo_area <- function(ifile, ofile = NULL) {
+  op <- rcdo::cdo_operator(command = globals$cdo_area, params = NULL, n_input = 1, n_output = 1)
+  rcdo::cdo(op, input = list(ifile), params = NULL, output = ofile)
 }
 
 cdo_iiee <- function(ifile1, ifile2, output, threshhold = 0.15) {
-        file1 <- rcdo::cdo_gtc(ifile1, c = threshhold) |> 
-                        rcdo::cdo_options_use("-L") |> 
-                        rcdo::cdo_execute()
+  file1 <- rcdo::cdo_gtc(ifile1, c = threshhold) |>
+    rcdo::cdo_options_use("-L") |>
+    rcdo::cdo_execute()
 
-        file2 <- rcdo::cdo_gtc(ifile2, c = threshhold) |> 
-                        rcdo::cdo_options_use("-L") |> 
-                        rcdo::cdo_execute()
+  file2 <- rcdo::cdo_gtc(ifile2, c = threshhold) |>
+    rcdo::cdo_options_use("-L") |>
+    rcdo::cdo_execute()
 
-        out <- rcdo::cdo_ne(file1, file2) |> 
-                rcdo::cdo_fldint()  |> 
-                rcdo::cdo_execute(output = output)
+  out <- rcdo::cdo_ne(file1, file2) |>
+    rcdo::cdo_fldint() |>
+    rcdo::cdo_execute(output = output)
 
-        file.remove(c(file1, file2))
-        return(out)
+  file.remove(c(file1, file2))
+  return(out)
 }
 
 
-anchor_limits <- function(anchor = 0, binwidth = NULL, exclude = NULL,  bins = 10,
+anchor_limits <- function(anchor = 0, binwidth = NULL, exclude = NULL, bins = 10,
                           range = c(NA, NA), sym = TRUE) {
   force(range)
   force(anchor)
@@ -57,60 +55,93 @@ anchor_limits <- function(anchor = 0, binwidth = NULL, exclude = NULL,  bins = 1
       D <- max(abs(x[1] - anchor), abs(x[2] - anchor))
       x <- c(anchor - D, anchor + D)
     }
-    
-    
+
+
     if (!is.null(binwidth)) binwidth2 <- binwidth
     if (is.null(binwidth2)) {
       binwidth2 <- diff(pretty(x, bins))[1]
     }
-    
-    mult <- ceiling((x[1] - anchor)/binwidth2) - 1L
-    start <- anchor + mult*binwidth2
+
+    mult <- ceiling((x[1] - anchor) / binwidth2) - 1L
+    start <- anchor + mult * binwidth2
     b <- seq(start, x[2] + binwidth2, binwidth2)
     b <- b[!(b %in% exclude)]
-    
+
     if (!is.na(range[1])) {
       m <- min(b)
-      b <- c(b[b >= (range[1] + 1e-9)], -Inf)  # DAMN YOU FLOATING POINT ERRORS!!! 
+      b <- c(b[b >= (range[1] + 1e-9)], -Inf) # DAMN YOU FLOATING POINT ERRORS!!!
     }
-    
+
     if (!is.na(range[2])) {
       m <- max(b)
       b <- c(b[b <= (range[2] - 1e-9)], Inf)
     }
     sort(b)
-    
   }
 }
 
 get <- `$`
 
-sic_projection <- globals$nsidc_climatology_monthly |>  
-  ncdf4::nc_open() |>  
-  ncdf4::ncatt_get(varid = "crs") |> 
+sic_projection <- globals$nsidc_climatology_monthly |>
+  here::here() |> 
+  ncdf4::nc_open() |>
+  ncdf4::ncatt_get(varid = "crs") |>
   get("proj_params")
 
-topo <- cdo_topo(grid = globals$nsidc_grid, 
-                 ofile = file.path(globals$data_derived, "topo.nc")) |> 
-    cdo_execute(options = c("-f nc")) |> 
-    ReadNetCDF(c(z = "topo")) |> 
-    _[, .(x = xgrid, y = ygrid, z)]
+topo <- cdo_topo(
+  grid = here::here(globals$nsidc_grid),
+  ofile = here::here(file.path(globals$data_derived, "topo.nc"))
+) |>
+  cdo_execute(options = c("-f nc")) |>
+  ReadNetCDF(c(z = "topo")) |>
+  _[, .(x = xgrid, y = ygrid, z)]
+
+
+theme_set(theme_minimal() +
+            theme(panel.background = element_rect(fill = "#fafafa", color = NA),
+                  legend.position = "bottom",
+                  legend.title.position = "top", 
+                  legend.title = element_text(hjust = 0.5),
+                  legend.frame = element_rect(color = "black", linewidth = 0.4),
+                  legend.key.height = unit(0.75, "lines")
+            ))
+wide_legend <- theme(legend.key.width = unit(1, 'null'))
 
 contour <- StatContour$compute_group(topo, breaks = 0)
 geom_antarctica_path <- geom_path(data = contour, aes(x, y, group = group), inherit.aes = FALSE, colour = "black")
 
-geom_antarctica_fill <- geom_polygon(data = contour, aes(x, y, group = group), inherit.aes = FALSE, colour = "black", fill ="#FAFAFA")
+geom_antarctica_fill <- geom_polygon(data = contour, aes(x, y, group = group), inherit.aes = FALSE, colour = "black", fill = "#FAFAFA")
 
-geomcoord_antarctica <- list(NULL
-    , coord_sf(crs = sic_projection, lims_method = "box")
-    , scale_x_continuous(name = NULL, expand = c(0, 0))
-    , scale_y_continuous(name = NULL, expand = c(0, 0))
-    , geom_antarctica_path
+geomcoord_antarctica <- list(
+  coord_sf(crs = sic_projection, lims_method = "box"),
+  scale_x_continuous(name = NULL, expand = c(0, 0)),
+  scale_y_continuous(name = NULL, expand = c(0, 0)),
+  geom_antarctica_path
 )
 
-scale_color_models <- scale_color_manual(NULL, 
-                     values = c(access = "black", 
-                                nsidc = "#e66100"),
-                     labels = c(access = "ACCESS-S2",
-                                nsidc = "NSIDC CDRV4"))
+scale_color_models <- scale_color_manual(NULL,
+  values = c(
+    access = "black",
+    nsidc = "#1a5fb4"
+  ),
+  labels = c(
+    access = "ACCESS-S2",
+    nsidc = "NSIDC CDRV4"
+  )
+)
 
+
+scale_fill_models <- scale_fill_manual(NULL,
+  values = c(
+    access = "black",
+    nsidc = "#1a5fb4"
+  ),
+  labels = c(
+    access = "ACCESS-S2",
+    nsidc = "NSIDC CDRV4"
+  )
+)
+
+
+labels_extent <- scales::label_number(scale = 1e-12)
+labels_month <- setNames(month.abb, 1:12)
